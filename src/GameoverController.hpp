126cf3a3b52d4c18c7115a45fb55ec673d4f1fbb
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2015-04-29 12:39:45 +0900

    game over UI修正

--- src/GameoverController.hpp
+++ src/GameoverController.hpp
@@ -15,6 +15,10 @@ class GameoverController : public ControllerBase {
   ci::JsonTree& params_;
   Event<EventParam>& event_;
 
+  float tween_delay_;
+  float event_delay_;
+  float deactive_delay_;
+
   std::unique_ptr<UIView> view_;
   
   bool active_;
@@ -31,6 +35,9 @@ public:
                      std::unique_ptr<UIView>&& view) :
     params_(params),
     event_(event),
+    tween_delay_(params["gameover.tween_delay"].getValue<float>()),
+    event_delay_(params["gameover.event_delay"].getValue<float>()),
+    deactive_delay_(params["gameover.deactive_delay"].getValue<float>()),
     view_(std::move(view)),
     active_(true),
     event_timeline_(ci::Timeline::create())
@@ -43,20 +50,20 @@ public:
 
     connections_ += event.connect("gameover-agree",
                                   [this](const Connection& connection, EventParam& param) {
-                                    // 時間差tween
                                     event_timeline_->add([this]() {
                                         view_->startWidgetTween("tween-out");
+
+                                        event_timeline_->add([this]() {
+                                            event_.signal("begin-title", EventParam());
+                                            
+                                            event_timeline_->add([this]() {
+                                                active_ = false;
+                                              },
+                                              event_timeline_->getCurrentTime() + deactive_delay_);
+                                          },
+                                          event_timeline_->getCurrentTime() + event_delay_);
                                       },
-                                      event_timeline_->getCurrentTime() + 0.5f);
-                                    
-                                    // 時間差でControllerを破棄
-                                    event_timeline_->add([this]() {
-                                        event_.signal("begin-title", EventParam());
-                                        active_ = false;
-                                      },
-                                      event_timeline_->getCurrentTime() + 1.0f);
-                                    
-                                    connection.disconnect();
+                                      event_timeline_->getCurrentTime() + tween_delay_);
                                   });
 
     view_->startWidgetTween("tween-in");
