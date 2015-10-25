cfffdf34c268322e1c8385ec0a9d3a0453dd2b8c
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2015-10-25 20:53:12 +0900

    実績リクエスト関数追加

---
+++ src/AchievementRequest.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+//
+// 実績の更新
+//
+
+#include "Event.hpp"
+#include "EventParam.hpp"
+
+
+namespace ngs {
+
+void AchievementRequest(Event<EventParam>& event,
+                        const std::string& id, const double value = 100.0) noexcept {
+  EventParam params = {
+    { "id",    id },
+    { "value", value },
+  };
+  event.signal("entry-achievement", params);
+}
+
+}
